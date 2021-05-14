4ab4efe005221e28965df8b559becac443ecc992
Sarath Sreedharan <sarathsreedharan@Saraths-MacBook-Pro.local>
2021-05-13 21:41:26 -0700

    fixing the column

--- papers/program.js
+++ papers/program.js
@@ -295,7 +295,7 @@ function render_keyword_columns() {
   topics_str = `
                 <a
                     href="javascript:update_paper_list_for_topics()"
-                    class="list-group-item list-group-keyword list-group-item-action d-flex justify-content-between align-items-start"
+                    class="list-group-item list-group-keyword list-group-item-action d-flex justify-content-between align-items-start@ACT@"
                     id="KEYWORD_@TPC@"
                   >
                     @TPC@
@@ -304,8 +304,15 @@ function render_keyword_columns() {
                  `;
   str = "";
   for (var tpc in keyword_cnt) {
+    active_str = "";
+    for (var id in keyword_filter) {
+      if (tpc == keyword_filter[id]) {
+        active_str = " active";
+      }
+    }
     str += topics_str
       .replaceAll("@TPC@", tpc)
+      .replaceAll("@ACT@", active_str)
       .replaceAll("@CNT@", keyword_cnt[tpc]);
   }
 
