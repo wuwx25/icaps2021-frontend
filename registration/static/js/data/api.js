2987eb25dcbb1b7e59fac967611e040ba7a6d9ad
YuyongHu <867115311@qq.com>
2021-06-15 19:41:46 +0800

    add registration page, no function

---
+++ registration/static/js/data/api.js
@@ -0,0 +1,108 @@
+/* eslint-disable no-underscore-dangle */
+class API {
+  /**
+   * get and cache config object
+   * @return object
+   */
+  static getConfig() {
+    if (API.configCache == null) {
+      API.configCache = $.get("./assets/data/serve_config.json");
+    }
+    return API.configCache;
+  }
+
+  static getCalendar() {
+    return $.get("serve_main_calendar.json");
+  }
+
+  static getPapers() {
+    //if (API.paperCache == null) {
+    API.paperCache = [];
+    for (var id in top.program) {
+      if (
+        top.program[id]["render"] == true &&
+        top.program[id]["render_track"] == true &&
+        top.program[id]["render_topics"] == true
+      ) {
+        API.paperCache.push(top.program[id]);
+      }
+    } //$.get("./assets/data/papers.json");
+    //}
+    return API.paperCache;
+  }
+
+  static getPapersAndProjection() {
+    return Promise.all([
+      API.getPapers(),
+      $.get("./assets/data/papers_projection.json"),
+    ]);
+  }
+
+  /**
+   * lazy store creation/loading - not needed if own store backend
+   * @see API.storeIDs
+   * @return object
+   */
+  static getStore(storeID) {
+    if (!(storeID in API._storeCaches)) {
+      API._storeCaches[storeID] = new Persistor(
+        `miniconf-${API.getConfig().name}-${storeID}`
+      );
+    }
+    return API._storeCaches[storeID];
+  }
+
+  /**
+   * get marks for all papers of a specific type
+   * @see API.storeIDs
+   * @param storeID
+   * @return {Promise<object>}
+   */
+  static async markGetAll(storeID) {
+    return API.getStore(storeID).getAll();
+  }
+
+  static async markSet(storeID, paperID, read = true) {
+    return API.getStore(storeID).set(paperID, read);
+  }
+
+  /*
+   * Resource paths
+   */
+
+  /**
+   * Link to thumbnails derived from paper object
+   * @param paper
+   * @return {string}
+   */
+  static thumbnailPath(paper) {
+    return `https://iclr.github.io/iclr-images/small/${paper.UID}.jpg`;
+  }
+
+  /**
+   * Link to poster detail derived from paper object
+   * @param paper
+   * @return {string}
+   */
+  static posterLink(paper) {
+    return `poster_${paper.UID}.html`;
+  }
+
+  /**
+   * link to the poster ICAL file for poster and repetition i
+   * @param paper
+   * @param i
+   * @return {string}
+   */
+  static posterICS(paper, i) {
+    return `webcal://iclr.github.io/iclr-images/calendars/poster_${paper.UID}.${i}.ics`;
+  }
+}
+
+API.configCache = null;
+API.paperCache = null;
+API._storeCaches = {};
+API.storeIDs = {
+  visited: "visited",
+  bookmarked: "bookmark",
+};
