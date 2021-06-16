5027520c6410c2db39738e7dbf195608a4744801
wx_cf3348ab12 <1934410205@qq.com>
2021-06-16 20:52:47 +0800

    modified some error

--- registration/index.js
+++ registration/index.js
@@ -3,12 +3,24 @@ var app = new Vue({
     data: {
       message: 'Hello Vue!',
       user_info:{
-
-      }
+          email: "",
+          first_name:"",
+          last_name:"",
+          pronoun:"",
+          institution:"",
+          password:"",
+          password2:"",
+          country:"hahah"
+      },
+	  test:"test"
     },
     mounted:function(){
+        console.log("这是moutned")
       axios.defaults.baseURL=window.location.protocol+"//"+window.location.hostname+":5438";
-      
-    }
-
+    },
+	methods:{
+		next:function (){
+		    console.log('next..............')
+		}
+	}
   })
