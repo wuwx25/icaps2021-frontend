b64801484a2e0ccd141cdfedab449fca74ad1ff5
wuweixin <2731472384@qq.com>
2021-07-12 07:46:51 +0000

    modify the rocketchat page

---
+++ exhibition/index.js
@@ -0,0 +1,24 @@
+import {backendBaseUrl} from '../assets/js/backendBaseUrl.js';
+import {country, isLogin} from '../assets/js/data.js';
+var app = new Vue({
+    el: '#app',
+    data: {
+        token     : "",
+        channel:'general'
+    },
+    methods: {
+
+    },
+    mounted: function() {
+        axios.defaults.withCredentials = true;
+        console.log("exhibition");
+        if(localStorage.getItem("channel")){
+            channel=localStorage.getItem("channel");
+        }
+    },
+    watch: {
+        token: async function () {
+            console.log(`token now is ${this.token}`);
+        },
+    }
+})
