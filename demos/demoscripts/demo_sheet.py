1529123f6ce3af79892c49bfe8c23f65c812a2cf
Ubuntu <yochan@yochan1.wq05meqkrcaetfjrraeacfwdxc.dx.internal.cloudapp.net>
2021-07-16 07:20:00 +0000

    making sure the return is json

--- demos/demoscripts/demo_sheet.py
+++ demos/demoscripts/demo_sheet.py
@@ -31,87 +31,87 @@ def index():
 @app.route("/fd")
 def fd_clicked():
     db.child("click_stats").push({"clicked":"fd", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
     
 @app.route("/tarski")
 def tarski_clicked():
     db.child("click_stats").push({"clicked":"tarski", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
 
 @app.route("/rosplan")
 def rosplan_clicked():
     db.child("click_stats").push({"clicked":"rosplan", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
 
 @app.route("/ros2")
 def ros2_clicked():
     db.child("click_stats").push({"clicked":"ros2", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
 
 @app.route("/val")
 def val_clicked():
     db.child("click_stats").push({"clicked":"val", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
  
 @app.route("/optic")
 def optic_clicked():
     db.child("click_stats").push({"clicked":"optic", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
  
 @app.route("/kcl_planner")
 def kcl_planner_clicked():
     db.child("click_stats").push({"clicked":"kcl_planner", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
  
 @app.route("/prp")
 def prp_clicked():
     db.child("click_stats").push({"clicked":"prp", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
  
 @app.route("/ff")
 def ff_clicked():
     db.child("click_stats").push({"clicked":"ff", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
  
 @app.route("/ibm_top_k")
 def ibm_top_k_clicked():
     db.child("click_stats").push({"clicked":"ibm_top_k", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
 
 @app.route("/pyperplan")
 def pyperplan_clicked():
     db.child("click_stats").push({"clicked":"pyperplan", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
  
 @app.route("/lapkt")
 def lapkt_clicked():
     db.child("click_stats").push({"clicked":"lapkt", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
  
 @app.route("/planutils")
 def planutils_clicked():
     db.child("click_stats").push({"clicked":"planutils", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
  
 @app.route("/planning_domains")
 def planning_domains_clicked():
     db.child("click_stats").push({"clicked":"planning_domains", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
  
 @app.route("/vs_code")
 def vs_code_clicked():
     db.child("click_stats").push({"clicked":"vs_code", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
  
 @app.route("/sublime")
 def sublime_clicked():
     db.child("click_stats").push({"clicked":"sublime", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
  
 @app.route("/atom")
 def atom_clicked():
     db.child("click_stats").push({"clicked":"atom", "timestamp":time.time()})
-    return "Updated"
+    return "['Updated']"
  
 if __name__ == "__main__":
     app.run()
